c0fdda5367c45527cd199d26c4afaf637382302e
Sudha Agarwal <sudha.agarwal84@gmail.com>
2024-07-21 21:02:05 +0530

    updated

--- Mocha/restapi-testing/mocha.config.js
+++ Mocha/restapi-testing/mocha.config.js
@@ -8,11 +8,3 @@ module.exports = {
     require:['./test/global-setup.mjs']
 }
 
-/*
-module.exports = {
-reporter: 'mocha-junit-reporter',
-  reporterOptions: {
-    mochaFile: './test-results.xml',
-  },
-  require:['./test/global-setup.mjs']
-}*/
